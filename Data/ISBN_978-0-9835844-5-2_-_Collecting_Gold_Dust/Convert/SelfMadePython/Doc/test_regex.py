7a8dac75269dcba3247535de2c46650ac9ee1ffb
EricBoix <eric.boix@insa-lyon.fr>
2026-05-28 14:19:36 +0200

    Collecting_Gold_Dust moved to new repository.

--- Data/ISBN_978-0-9835844-5-2_-_Collecting_Gold_Dust/Convert/SelfMadePython/Doc/test_regex.py
+++
@@ -1,7 +0,0 @@
-import re
-s = 'I am \nstruggling to\n\n\n to make this this work'
-match = re.search(r'(\n){2}(?!(\n))', s)
-print("No return                   ", re.search(r'(?!(\n))', s))
-print("Two returns (at least)      ", re.search(r'(\n){2}', s))
-print("Exactly two, not 1 and NOT 3", re.search(r'(\n){2}(?!(\n))', s))
-print("Exactly two, not 1 and NOT 3", re.search(r'(?<!(\n))(\n){2}(?!(\n))', s))
